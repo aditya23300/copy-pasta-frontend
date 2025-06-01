04433b6e5863364df98ab19b957cbe85baa115fe
Aditya Raj <adityaraj@smcfinance.com>
2025-06-02 00:13:41 +0530

    logs removed

--- src/app/services/data-transfer.service.ts
+++ src/app/services/data-transfer.service.ts
@@ -9,15 +9,15 @@ import { environment } from '../../environments/environment';
 export class DataTransferService {
   constructor(private http: HttpClient) {}
   sendData(data: any): Observable<any> {
-    console.log('api call initiated with payload', data);
+   // console.log('api call initiated with payload', data);
 
     const url = `${environment.apiUrl}/data-transfer/send-data`;
-    console.log('the url from the env is:', `${environment.apiUrl}`);
+  //  console.log('the url from the env is:', `${environment.apiUrl}`);
     return this.http.post(url, data);
   }
   receiveData(docID: string): Observable<any> {
     const url = `${environment.apiUrl}/data-transfer/${docID}`;
-    console.log('the url from the env is:', `${environment.apiUrl}`);
+   // console.log('the url from the env is:', `${environment.apiUrl}`);
     return this.http.get(url);
   }
 }
